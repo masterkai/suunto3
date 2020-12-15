2b01782d584c3a5da54c9b78a9e3b0b88927178d
max <max@verygps.com.tw>
2020-12-15 17:12:21 +0800

    add sticky nav & suunto-app section

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger)
 
 function initHeaderTilt() {
-  document.querySelector("main").addEventListener("mousemove", moveImages);
+  document.querySelector('main').addEventListener('mousemove', moveImages)
 }
 
 function moveImages(e) {
-  const { offsetX, offsetY, target } = e;
-  const { clientWidth, clientHeight } = target;
+  const { offsetX, offsetY, target } = e
+  const { clientWidth, clientHeight } = target
 
   // console.log(clientWidth, clientHeight);
-  const xPos = offsetX / clientWidth - 0.5;
-  const yPos = offsetY / clientHeight - 0.5;
-  const leftImage = gsap.utils.toArray(".hg__left .hg__image");
-  const rightImage = gsap.utils.toArray(".hg__right .hg__image");
-  const perspective = document.querySelector(".product__img__perspective img");
-  const front = document.querySelector(".product__img__FrontView img");
-  const hgLeftL = document.querySelector(".hg__left .hg__image--l img");
-  const hgLeftM = document.querySelector(".hg__left .hg__image--m img");
-  const hgLeftS = document.querySelector(".hg__left .hg__image--s img");
-  const hgRightL = document.querySelector(".hg__right .hg__image--l img");
-  const hgRightM = document.querySelector(".hg__right .hg__image--m img");
-  const hgRightS = document.querySelector(".hg__right .hg__image--s img");
-  const header = document.querySelector("header");
+  const xPos = offsetX / clientWidth - 0.5
+  const yPos = offsetY / clientHeight - 0.5
+  const leftImage = gsap.utils.toArray('.hg__left .hg__image')
+  const rightImage = gsap.utils.toArray('.hg__right .hg__image')
+  const perspective = document.querySelector('.product__img__perspective img')
+  const front = document.querySelector('.product__img__FrontView img')
+  const hgLeftL = document.querySelector('.hg__left .hg__image--l img')
+  const hgLeftM = document.querySelector('.hg__left .hg__image--m img')
+  const hgLeftS = document.querySelector('.hg__left .hg__image--s img')
+  const hgRightL = document.querySelector('.hg__right .hg__image--l img')
+  const hgRightM = document.querySelector('.hg__right .hg__image--m img')
+  const hgRightS = document.querySelector('.hg__right .hg__image--s img')
+  const header = document.querySelector('header')
 
   // console.log(target)
   // console.log(target)
   if (target !== header) {
-    header.style.backgroundColor = "#F4F4F4";
+    header.style.backgroundColor = '#F4F4F4'
 
     leftImage.forEach((image, index) => {
       gsap.to(image, {
@@ -35,9 +35,9 @@ function moveImages(e) {
         y: 0,
         rotationX: 0,
         rotationY: 0,
-        ease: "Power3.out",
-      });
-    });
+        ease: 'Power3.out',
+      })
+    })
 
     rightImage.forEach((image, index) => {
       gsap.to(image, {
@@ -46,33 +46,33 @@ function moveImages(e) {
         y: 0,
         rotationX: 0,
         rotationY: 0,
-        ease: "Power3.out",
-      });
-    });
+        ease: 'Power3.out',
+      })
+    })
   } else {
     if (xPos < 0) {
-      perspective.src = "/img/SUUNTO-3-SLATE-GREY-COPPER-Perspective-View.png";
-      front.src = "/img/SUUNTO-3-GRANITE-RED-Front-View.png";
-      hgLeftL.src = "/img/suunto3_on-wrist_weekly_training_plan.jpg";
-      hgLeftM.src = "/img/suunto3_social3.jpg";
-      hgLeftS.src = "/img/suunto3_work_meeting5.jpg";
-      hgRightL.src = "/img/suunto3_on-wrist_watchface_coffee3.jpg";
-      hgRightM.src = "/img/suunto3_work_meeting4.jpg";
-      hgRightS.src = "/img/suunto3_social9.jpg";
-      header.style.backgroundColor = "#FBECEC";
+      perspective.src = '/img/SUUNTO-3-SLATE-GREY-COPPER-Perspective-View.png'
+      front.src = '/img/SUUNTO-3-GRANITE-RED-Front-View.png'
+      hgLeftL.src = '/img/suunto3_on-wrist_weekly_training_plan.jpg'
+      hgLeftM.src = '/img/suunto3_social3.jpg'
+      hgLeftS.src = '/img/suunto3_work_meeting5.jpg'
+      hgRightL.src = '/img/suunto3_on-wrist_watchface_coffee3.jpg'
+      hgRightM.src = '/img/suunto3_work_meeting4.jpg'
+      hgRightS.src = '/img/suunto3_social9.jpg'
+      header.style.backgroundColor = '#FBECEC'
     } else {
-      perspective.src = "/img/SUUNTO-3-SLATE-GREY-Perspective-View.png";
-      front.src = "/img/SUUNTO-3-MOSS-GREY-Front-View.png";
-      hgLeftL.src = "/img/suunto3_swim4_4x5.jpg";
-      hgLeftM.src = "/img/suunto3_commute5.jpg";
-      hgLeftS.src = "/img/suunto3_commute2.jpg";
-      hgRightL.src = "/img/suunto3_run5.jpg";
-      hgRightM.src = "/img/suunto3_run3.jpg";
-      hgRightS.src = "/img/suunto3_run4.jpg";
-      header.style.backgroundColor = "#F4F4F4";
+      perspective.src = '/img/SUUNTO-3-SLATE-GREY-Perspective-View.png'
+      front.src = '/img/SUUNTO-3-MOSS-GREY-Front-View.png'
+      hgLeftL.src = '/img/suunto3_swim4_4x5.jpg'
+      hgLeftM.src = '/img/suunto3_commute5.jpg'
+      hgLeftS.src = '/img/suunto3_commute2.jpg'
+      hgRightL.src = '/img/suunto3_run5.jpg'
+      hgRightM.src = '/img/suunto3_run3.jpg'
+      hgRightS.src = '/img/suunto3_run4.jpg'
+      header.style.backgroundColor = '#F4F4F4'
     }
 
-    const modifier = (index) => index * 1.2 + 0.5;
+    const modifier = (index) => index * 1.2 + 0.5
 
     //move 3 left images
     //get 0 0 in the middle screen
@@ -83,9 +83,9 @@ function moveImages(e) {
         y: yPos * 30 * modifier(index),
         rotationX: yPos * 10,
         rotationY: xPos * 40,
-        ease: "Power3.out",
-      });
-    });
+        ease: 'Power3.out',
+      })
+    })
 
     rightImage.forEach((image, index) => {
       gsap.to(image, {
@@ -94,27 +94,27 @@ function moveImages(e) {
         y: -yPos * 30 * modifier(index),
         rotationX: yPos * 10,
         rotationY: xPos * 40,
-        ease: "Power3.out",
-      });
-    });
+        ease: 'Power3.out',
+      })
+    })
   }
 }
 
 function init() {
   // start here
-  initHeaderTilt();
+  initHeaderTilt()
 }
 
-window.addEventListener("load", function () {
-  init();
-});
+window.addEventListener('load', function () {
+  init()
+})
 
-const slides = document.querySelectorAll(".swiper-container01 .swiper-slide");
+const slides = document.querySelectorAll('.swiper-container01 .swiper-slide')
 
-const sliderSelector01 = ".swiper-container01";
+const sliderSelector01 = '.swiper-container01'
 const options = {
   // Optional parameters
-  direction: "horizontal",
+  direction: 'horizontal',
   init: false,
   loop: true,
   speed: 900,
@@ -126,41 +126,43 @@ const options = {
 
   // If we need pagination
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   on: {
     slideChangeTransitionStart: function () {
       slides.forEach((slide) => {
-        slide.classList.remove("active");
-      });
-      slides[this.realIndex].classList.add("active");
+        slide.classList.remove('active')
+      })
+      slides[this.realIndex].classList.add('active')
       /* do something */
       // console.log(this.activeIndex)
     },
   },
-};
-const mySwiper = new Swiper(sliderSelector01, options);
+}
+const mySwiper = new Swiper(sliderSelector01, options)
 
-mySwiper.init();
+mySwiper.init()
 
-const activeTitle = document.getElementById("activeTitle");
-const sliderContainer02 = document.querySelector(".swiper-container02");
-const sliderSelector02 = ".swiper-container02";
+const activeTitle = document.getElementById('activeTitle')
+const sliderContainer02 = document.querySelector('.swiper-container02')
+const sliderSelector02 = '.swiper-container02'
 const options2 = {
   init: false,
   loop: true,
   speed: 800,
   slidesPerView: 1,
   spaceBetween: 0,
-
+  autoplay: {
+    delay: 5000,
+  },
   centeredSlides: true,
-  effect: "coverflow", // 'cube', 'fade', 'coverflow',
+  effect: 'coverflow', // 'cube', 'fade', 'coverflow',
   coverflowEffect: {
     rotate: 50, // Slide rotate in degrees
     stretch: 10, // Stretch space between slides (in px)
@@ -175,15 +177,15 @@ const options2 = {
     clickable: false,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   breakpoints: {
     768: {
       slidesPerView: 2, // or 'auto'
       spaceBetween: 50,
       pagination: {
-        el: ".swiper-pagination",
+        el: '.swiper-pagination',
         clickable: true,
       },
     },
@@ -193,173 +195,189 @@ const options2 = {
     transitionStart: function (params) {
       // console.log(this.realIndex);
       if (this.realIndex < 3) {
-        activeTitle.textContent = "為您量身訂做的【智能訓練導引】";
+        activeTitle.textContent = '為您量身訂做的【智能訓練導引】'
       } else {
-        activeTitle.textContent = "滿足您全方位的運動需求";
+        activeTitle.textContent = '滿足您全方位的運動需求'
       }
     },
     imagesReady: function () {
-      this.el.classList.remove("loading");
+      this.el.classList.remove('loading')
     },
   },
-};
+}
 
-const mySwiper2 = new Swiper(sliderSelector02, options2);
+const mySwiper2 = new Swiper(sliderSelector02, options2)
 
-mySwiper2.init();
+mySwiper2.init()
 
 const app = Vue.createApp({
   data() {
     return {
       icons: [
         {
-          name: "iconfont icon_activity_tracker",
-          feature: "全天候活動監測",
-          img: "/img/features_activity_tracker.png",
+          name: 'iconfont icon_activity_tracker',
+          feature: '全天候活動監測',
+          img: '/img/features_activity_tracker.png',
         },
         {
-          name: "iconfont icon_adaptive_training_guidance",
-          feature: "智能導引訓練",
-          img: "/img/features_adaptive_training_guidance.png",
+          name: 'iconfont icon_adaptive_training_guidance',
+          feature: '智能導引訓練',
+          img: '/img/features_adaptive_training_guidance.png',
         },
         {
-          name: "iconfont icon_fitness_level",
-          feature: "體適能水準",
-          img: "/img/features_fitness_level.png",
+          name: 'iconfont icon_fitness_level',
+          feature: '體適能水準',
+          img: '/img/features_fitness_level.png',
         },
         {
-          name: "iconfont icon_sleep",
-          feature: "睡眠品質監測",
-          img: "/img/features_sleep.png",
+          name: 'iconfont icon_sleep',
+          feature: '睡眠品質監測',
+          img: '/img/features_sleep.png',
         },
         {
-          name: "iconfont icon_wireless_mobile_connection",
-          feature: "連接手機GPS",
-          img: "/img/features_wireless_mobile_connection.png",
+          name: 'iconfont icon_wireless_mobile_connection',
+          feature: '連接手機GPS',
+          img: '/img/features_wireless_mobile_connection.png',
         },
         {
-          name: "iconfont icon_water_resistant_30m",
-          feature: "水下30米防水等級",
-          img: "/img/features_water_resistant_30m.png",
+          name: 'iconfont icon_water_resistant_30m',
+          feature: '水下30米防水等級',
+          img: '/img/features_water_resistant_30m.png',
         },
         {
-          name: "iconfont icon_wrist_based_heart_rate",
-          feature: "腕式光學心率",
-          img: "/img/features_wrist_based_heart_rate.png",
+          name: 'iconfont icon_wrist_based_heart_rate',
+          feature: '腕式光學心率',
+          img: '/img/features_wrist_based_heart_rate.png',
         },
         {
-          name: "iconfont icon_stress",
-          feature: "疲勞壓力與恢復狀態",
-          img: "/img/features_stress.png",
+          name: 'iconfont icon_stress',
+          feature: '疲勞壓力與恢復狀態',
+          img: '/img/features_stress.png',
         },
       ],
-      original: "/img/SUUNTO-3-SLATE-GREY-COPPER-Front-View.png",
-      imgSRC: "/img/SUUNTO-3-SLATE-GREY-COPPER-Front-View.png",
-    };
+      original: '/img/SUUNTO-3-SLATE-GREY-COPPER-Front-View.png',
+      imgSRC: '/img/SUUNTO-3-SLATE-GREY-COPPER-Front-View.png',
+    }
   },
   methods: {
     changeFeature(img) {
-      this.imgSRC = img;
+      this.imgSRC = img
     },
     toOriginal() {
-      this.imgSRC = this.original;
+      this.imgSRC = this.original
     },
   },
-});
+})
 
-app.mount("#features");
+app.mount('#features')
 
 $(function () {
-  var windowWidth = $(window).width();
-  var windowHeight = $(window).height();
+  // stiky nav
+  const nav = document.querySelector('nav')
+  const topofNav = nav.offsetTop
+  function fixNav() {
+    if (window.scrollY >= topofNav) {
+      document.body.style.paddingTop = nav.offsetHeight + 'px'
+      document.body.classList.add('fixed-nav')
+    } else {
+      document.body.style.paddingTop = 0
+      document.body.classList.remove('fixed-nav')
+    }
+  }
+
+  window.addEventListener('scroll', fixNav)
+
+  var windowWidth = $(window).width()
+  var windowHeight = $(window).height()
 
   //WOW
-  new WOW().init();
+  new WOW().init()
 
   var controller = new ScrollMagic.Controller({
     addIndicators: false,
-  });
+  })
 
-  $("#product-common-1 .slide").each(function () {
-    var featuresId = "#" + $(this).attr("id");
+  $('#product-common-1 .slide').each(function () {
+    var featuresId = '#' + $(this).attr('id')
     var featuresFirstId =
-      "#" +
-      $("#product-common-1 #slideContainer>.slide:first-child()").attr("id");
+      '#' +
+      $('#product-common-1 #slideContainer>.slide:first-child()').attr('id')
     var features = new TimelineMax({
       repeat: 0,
-    });
+    })
 
     if ($(window).width() >= 1024) {
       features
-        .to($(".slide .slide-right"), 1, { display: "none", opacity: 0 }, 1)
+        .to($('.slide .slide-right'), 1, { display: 'none', opacity: 0 }, 1)
         .to(
-          $(featuresId + " .slide-right"),
+          $(featuresId + ' .slide-right'),
           1,
-          { display: "block", opacity: 1 },
+          { display: 'block', opacity: 1 },
           1
-        );
+        )
 
       var featuresScene = new ScrollMagic.Scene({
         triggerElement: featuresId,
-        offset: -$(featuresId).height() / 4,
+        offset: -$(featuresId).height() / 1.3,
         duration: $(featuresId).height(),
       })
         .setTween(features)
-        .addTo(controller);
+        .addTo(controller)
     } else {
       features
         .to(
-          $(".slide .slide-right, .slide .slide-left"),
+          $('.slide .slide-right, .slide .slide-left'),
           1,
-          { display: "none", opacity: 0 },
+          { display: 'none', opacity: 0 },
           1
         )
         .to(
-          $(featuresId + " .slide-right, " + featuresId + " .slide-left"),
+          $(featuresId + ' .slide-right, ' + featuresId + ' .slide-left'),
           1,
-          { display: "block", opacity: 1 },
+          { display: 'block', opacity: 1 },
           1
-        );
+        )
 
       var featuresScene = new ScrollMagic.Scene({
         triggerElement: featuresId,
-        offset: -$(featuresId).height() / 3,
+        offset: -$(featuresId).height() / 5,
         duration: $(featuresId).height(),
       })
         .setTween(features)
-        .addTo(controller);
+        // .addIndicators()
+        .addTo(controller)
     }
-  });
+  })
 
   var leaveFeaturesInfo = new TimelineMax({
     repeat: 0,
-  });
+  })
 
   if (windowWidth >= 1024) {
-    leaveFeaturesInfo.to(".slide .slide-right, .slide .slide-left ", 1, {
-      top: "-100vh",
-    });
+    leaveFeaturesInfo.to('.slide .slide-right, .slide .slide-left ', 1, {
+      top: '-100vh',
+    })
 
     var leaveFeaturesInfoScene = new ScrollMagic.Scene({
-      triggerElement: "#mct",
-      duration: $("#mct").height() * 2.2,
+      triggerElement: '#sleep',
+      duration: $('#sleep').height() * 2.2,
     })
       .setTween(leaveFeaturesInfo)
-      .triggerHook("onLeave")
-      .addTo(controller);
+      .triggerHook('onLeave')
+      .addTo(controller)
   } else {
     leaveFeaturesInfo.to(
-      "#product-common-1 #slideContainer .slide .slide-wrapper>* ",
+      '#product-common-1 #slideContainer .slide .slide-wrapper>* ',
       1,
       { opacity: 0 }
-    );
+    )
 
     var leaveFeaturesInfoScene = new ScrollMagic.Scene({
-      triggerElement: "#mct",
+      triggerElement: '#sleep',
       duration: ($(window).height() / 3) * 3,
     })
       .setTween(leaveFeaturesInfo)
-      .triggerHook("onLeave")
-      .addTo(controller);
+      .triggerHook('onLeave')
+      .addTo(controller)
   }
-});
+})
